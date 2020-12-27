7a16c29d2feefe91aa9e5a116cdd7cb415173b87
iliasbaker <oelrekab2@gmail.com>
2020-12-27 19:22:59 +1300

    add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+alias c='clear'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias "\~"="cd ~"
+alias sudo='sudo '
+alias spicetify='~/spicetify-cli/spicetify'
+set fish_greeting
+alias v='vim'
+alias h='history'
+alias config='/usr/bin/git --git-dir=/home/leo/.cfg/ --work-tree=/home/leo'
